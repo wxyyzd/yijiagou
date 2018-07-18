5c83323528ce4496ae8577ebf4f1643517a049bf
zhuyonghui <1615322169@qq.com>
2018-07-19 07:45:25 +0800

    my完成

--- src/component/footer/index.js
+++ src/component/footer/index.js
@@ -9,10 +9,10 @@ class ShoppingCar extends Component{
 				<aside>
 
 					<ul>
-					 <li><NavLink to="/home" activeClassName="active">首页</NavLink></li>
-					 <li><NavLink to="/classify" activeClassName="active">分类</NavLink></li>
-					 <li><NavLink to="/shoppingCar" activeClassName="active">购物车</NavLink></li>
-					 <li><NavLink to="/my" activeClassName="active">我的易家</NavLink></li>
+					 <li><NavLink to="/home" activeClassName="active"><b className="iconfont icon-shouye"></b>首页</NavLink></li>
+					 <li><NavLink to="/classify" activeClassName="active"><b className="iconfont icon-fenlei"></b>分类</NavLink></li>
+					 <li><NavLink to="/shoppingCar" activeClassName="active"><b className="iconfont icon-gouwuche1"></b>购物车</NavLink></li>
+					 <li><NavLink to="/my" activeClassName="active"><b className="iconfont icon-wode"></b>我的易家</NavLink></li>
 					
 					</ul>
 				</aside>
--- src/component/navbar/index.js
+++ src/component/navbar/index.js
@@ -1,23 +1,22 @@
 import React,{Component} from "react";
 import "./index.css";
 import {NavLink} from 'react-router-dom';
-import Search from "../search";
+
 import 'antd-mobile/dist/antd-mobile.css';
 class Navbar extends Component{
 
 	render(){
 		return <div id="navbar">
-					<div className="left">
-				<NavLink to="/area" activeClassName="active">地区</NavLink>
-					</div>
-					<div className="right">
-				<NavLink to="/search" activeClassName="active">
-				<div className="inner">
-				   <Search/>
-				  </div> 
-				</NavLink>   
+		<ul>
+			<li><NavLink to="/area" activeClassName="active">地区</NavLink></li>
+			<li><NavLink to="/search" activeClassName="active"> <input type="text" placeholder="搜索商品" className="input"/></NavLink></li>
+			<li><NavLink to="/login" activeClassName="active">消息</NavLink></li>
+			
 
-			</div>	
+		</ul>
+
+
+
 		</div>
 	}
 }
